8bddac4e4b53c3f5bc4a788fbc6c4e7aa86d8e1f
Chirkin Ivan <fruitin3d@yandex.ru>
2023-02-02 19:06:49 +0700

    refactor: переименованы наименования Constraint

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS items
     user_id      bigint,
     request_id   bigint,
     CONSTRAINT items_pkey PRIMARY KEY (item_id),
-    CONSTRAINT fk_owner
+    CONSTRAINT fk_item_owner
         FOREIGN KEY (user_id)
             REFERENCES users (user_id)
 );
@@ -31,10 +31,10 @@ CREATE TABLE IF NOT EXISTS bookings
     end_time   timestamp   NOT NULL,
     CONSTRAINT bookings_pkey PRIMARY KEY (booking_id),
     CONSTRAINT check_dates check (start_time > now() AND start_time < end_time),
-    CONSTRAINT fk_item
+    CONSTRAINT fk_booking_item
         FOREIGN KEY (item_id)
             REFERENCES items (item_id),
-    CONSTRAINT fk_booker
+    CONSTRAINT fk_booking_booker
         FOREIGN KEY (user_id)
             REFERENCES users (user_id)
 );
@@ -45,7 +45,7 @@ CREATE TABLE IF NOT EXISTS requests
     description varchar(400) NOT NULL,
     user_id     bigint       NOT NULL,
     CONSTRAINT requests_pkey PRIMARY KEY (request_id),
-    CONSTRAINT fk_requester
+    CONSTRAINT fk_request_requester
         FOREIGN KEY (user_id)
             REFERENCES users (user_id)
 );
@@ -58,10 +58,10 @@ CREATE TABLE IF NOT EXISTS comments
     comment_text varchar(200) NOT NULL,
     created   timestamp    NOT NULL,
     CONSTRAINT comments_pkey PRIMARY KEY (comment_id),
-    CONSTRAINT fk_item
+    CONSTRAINT fk_comment_item
         FOREIGN KEY (item_id)
             REFERENCES items (item_id),
-    CONSTRAINT fk_author
+    CONSTRAINT fk_comment_author
         FOREIGN KEY (author_id)
             REFERENCES users (user_id)
 );
